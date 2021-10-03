23bc7987ae351614fe9eb066c4b89eb67bf8785b
tomdevisser <tom.devisser@mail.com>
2021-10-03 15:38:55 +0200

    Added temporary icons, Trello pages and extra WordPress bookmarks

--- options.js
+++ options.js
@@ -1,20 +1,34 @@
 $( function() {
-	chrome.storage.sync.get( [ 'devUrl', 'xdUrl' ], function( urls ) {
-		console.log( urls.devUrl );
+	chrome.storage.sync.get( [ 'devUrl', 'xdUrl', 'trelloInternUrl', 'trelloExternUrl' ], function( urls ) {
 		if ( urls.devUrl ) {
 			$( '#dev-url' ).val( urls.devUrl );
+		}
+
+		if ( urls.xdUrl ) {
 			$( '#xd-url' ).val( urls.xdUrl );
+		}
+
+		if ( urls.trelloExternUrl ) {
+			$( '#trello-e-url' ).val( urls.trelloExternUrl );
+		}
+
+		if ( urls.trelloInternUrl ) {
+			$( '#trello-i-url' ).val( urls.trelloInternUrl );
 		}
 	} )
 
 	$( '#save-urls' ).click( function() {
 		let devUrl = $( '#dev-url' ).val();
 		let xdUrl = $( '#xd-url' ).val();
+		let trelloInternUrl = $( '#trello-i-url' ).val();
+		let trelloExternUrl = $( '#trello-e-url' ).val();
 
 		if ( devUrl ) {
 			chrome.storage.sync.set( {
 				'devUrl': devUrl,
 				'xdUrl': xdUrl,
+				'trelloInternUrl': trelloInternUrl,
+				'trelloExternUrl': trelloExternUrl,
 			},
 			function() {
 				alert( 'New URL\'s saved' );
--- popup.js
+++ popup.js
@@ -1,12 +1,31 @@
 $( function() {
 
-	chrome.storage.sync.get( [ 'devUrl', 'xdUrl' ], function( urls ) {
+	chrome.storage.sync.get( [ 'devUrl', 'xdUrl', 'trelloInternUrl', 'trelloExternUrl' ], function( urls ) {
 		if ( urls.devUrl ) {
 			$( '#dev-home' ).attr( 'href', urls.devUrl );
 			$( '#dev-home' ).css('display', 'block');
 
 			$( '#dev-admin' ).attr( 'href', urls.devUrl + 'wp-admin' );
 			$( '#dev-admin' ).css('display', 'block');
+
+			$( '#dev-pages' ).attr( 'href', urls.devUrl + 'wp-admin/edit.php?post_type=page' );
+			$( '#dev-pages' ).css('display', 'block');
+
+			$( '#dev-facet' ).attr( 'href', urls.devUrl + 'wp-admin/options-general.php?page=facetwp' );
+			$( '#dev-facet' ).css('display', 'block');
+
+			$( '#dev-global' ).attr( 'href', urls.devUrl + 'wp-admin/admin.php?page=strl-general-settings' );
+			$( '#dev-global' ).css('display', 'block');
+		}
+
+		if ( urls.trelloInternUrl ) {
+			$( '#dev-trello-i' ).attr( 'href', urls.devUrl + 'wp-admin/admin.php?page=strl-general-settings' );
+			$( '#dev-trello-i' ).css('display', 'block');
+		}
+
+		if ( urls.trelloExternUrl ) {
+			$( '#dev-trello-e' ).attr( 'href', urls.devUrl + 'wp-admin/admin.php?page=strl-general-settings' );
+			$( '#dev-trello-e' ).css('display', 'block');
 		}
 
 		if ( urls.xdUrl ) {
